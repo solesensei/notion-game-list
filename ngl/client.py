83fba209b4c35d0fa0924e8d21b2fd2f878ab17d
SoleSensei <dimargfd@gmail.com>
2020-07-13 14:35:05 +0300

    upd

--- ngl/client.py
+++ ngl/client.py
@@ -1,4 +1,6 @@
+from notion.block import CollectionViewPageBlock
 from notion.client import NotionClient
+from notion.operations import build_operation
 
 from .utils import echo, color
 
@@ -7,6 +9,7 @@ class NotionGameList:
 
     def __init__(self, token_v2):
         self.client = NotionClient(token_v2=token_v2)
+        self._icon = "👾"
 
     @classmethod
     def login(cls, token_v2=None):
@@ -16,3 +19,110 @@ class NotionGameList:
             echo("Get 'token_v2' from browser cookies")
             token_v2 = input(color.c("Token: ")).strip()
         return cls(token_v2=token_v2)
+
+    def create_game_page(self, name: str = "Notion Game List"):
+        page = self.client.current_space.add_page(name)
+        v = page.children.add_new(CollectionViewPageBlock)
+        v.collection = self.client.get_collection(self.client.create_record("collection", parent=v, schema=self._game_list_schema()))
+        v.title = "Game List"
+        v.description = "My game list"
+        view = v.views.add_new(view_type="table")
+        with self.client.as_atomic_transaction():
+            self.client.submit_transaction(
+                build_operation(view.id, path=[], command="update", args=self._properites_format(), table="collection_view")
+            )
+        with ngl.client.as_atomic_transaction():
+            self.client.submit_transaction(
+                build_operation(v.collection.id, path=["icon"], command="set", args=self._icon, table="collection")
+            )
+        return v
+
+    def connect_page(self, url):
+        pass
+
+    def _game_list_schema(self):
+        return {
+            "title": {"name": "Title", "type": "title"},
+            "status": {
+                "name": "Status",
+                "type": "select",
+                "options": [
+                    {
+                        "color": "green",
+                        "value": "Completed"
+                    }
+                ]
+            },
+            "score": {
+                "name": "Score",
+                "type": "select",
+                "options": [
+                    {
+                        "color": "green",
+                        "value": "10",
+                    },
+                    {
+                        "color": "red",
+                        "value": "1",
+                    }
+                ],
+            },
+            "platforms": {
+                "name": "Platforms",
+                "type": "multi_select",
+                "options": [
+                    {
+                        "color": "gray",
+                        "value": "Steam",
+                    },
+                    {
+                        "color": "red",
+                        "value": "Switch",
+                    },
+                    {
+                        "color": "blue",
+                        "value": "PS4",
+                    },
+                ],
+            },
+            "notes": {"name": "Notes", "type": "text"},
+            "time": {"name": "Time", "type": "date"},
+        }
+
+    def _properites_format(self):
+        return {
+            "format": {
+                "table_properties": [
+                    {
+                        "property": "title",
+                        "visible": True,
+                        "width": 280
+                    },
+                    {
+                        "property": "status",
+                        "visible": True,
+                        "width": 100
+                    },
+                    {
+                        "property": "score",
+                        "visible": True,
+                        "width": 100
+                    },
+                    {
+                        "property": "platforms",
+                        "visible": True,
+                        "width": 200
+                    },
+                    {
+                        "property": "time",
+                        "visible": True,
+                        "width": 200
+                    },
+                    {
+                        "property": "notes",
+                        "visible": True,
+                        "width": 200
+                    }
+                ]
+            }
+        }
